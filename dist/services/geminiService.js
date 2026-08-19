1ee50a1cb7addec9961a961d411b40ef2d3f61c0
Samuel Huamán <100598639+SamuelHuamanDev@users.noreply.github.com>
2026-08-19 16:19:04 -0500

    Delete dist directory

--- dist/services/geminiService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MODEL_NAME = exports.ai = void 0;
-const genai_1 = require("@google/genai");
-const env_1 = require("../config/env");
-// Instanciamos el SDK oficial de Google usando nuestra API Key
-exports.ai = new genai_1.GoogleGenAI({ apiKey: env_1.config.geminiApiKey });
-exports.MODEL_NAME = 'gemini-3.5-flash-lite';
